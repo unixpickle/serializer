6a2407aea50c25ce30a9beea6e42f1fdf30ad2b9
Alex Nichol <unixpickle@gmail.com>
2016-06-15 10:49:14 -0400

    added int serializer

--- primitives.go
+++ primitives.go
@@ -1,5 +1,7 @@
 package serializer
 
+import "strconv"
+
 // Bytes is a serializable version of a byte slice.
 type Bytes []byte
 
@@ -11,8 +13,26 @@ func (b Bytes) SerializerType() string {
 	return "[]byte"
 }
 
+// Int is a serializer version of an int.
+type Int int
+
+func (i Int) Serialize() ([]byte, error) {
+	return []byte(strconv.Itoa(int(i))), nil
+}
+
+func (i Int) SerializerType() string {
+	return "int"
+}
+
 func init() {
 	RegisterDeserializer(Bytes(nil).SerializerType(), func(d []byte) (Serializer, error) {
 		return Bytes(d), nil
+	})
+	RegisterDeserializer(Int(0).SerializerType(), func(d []byte) (Serializer, error) {
+		num, err := strconv.Atoi(string(d))
+		if err != nil {
+			return nil, err
+		}
+		return Int(num), nil
 	})
 }
